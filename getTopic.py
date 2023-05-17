dc4eca97bc7be2ed44524df97209c99798e75c9b
AyushVdoit <ayushjaiswal.vdoit@gmail.com>
2023-05-17 17:06:57 +0530

    "getTopic updated"

--- getTopic.py
+++ getTopic.py
@@ -3,61 +3,65 @@ import boto3
 from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
-Topic=dynamodb.Table('Topic')
-Token_Data = dynamodb.Table('Token')
+topic = dynamodb.Table('Topic')
+token_data = dynamodb.Table('Token')
+
 
 def token_checker(token):
-	data=Token_Data.get_item(Key={'token' : token})
-	if 'Item' in data:
-		return True
-	else:
-		return False  
-	
+    data = token_data.get_item(Key={'token': token})
+    if 'Item' in data:
+        return True
+    else:
+        return False
+
 # to get topic info with given input topic id
+
+
 def lambda_handler(event, context):
-	try:
-		
-		data =event
-		required_fields = ["token","Topic_id"]
-		for field in required_fields:
-			if field not in data or not data[field]:
-				return {
-				'statusCode': 400,
-				'body': f'Error: {field} is required and cannot be empty'
-				}
-		
-		if token_checker(event['token']):
-			topic_info =Topic.get_item(Key={'Topic_id':event['Topic_id']})
-			if 'Item' in topic_info:
-				size = len(topic_info['Item']['question'])
-				topics=[]
-				for topic_question in topic_info['Item']['question']:
-					correct_answer=topic_question['correctAnswer']
-					topic_question['correctAnswer']=correct_answer.split("7481903939")[0]
-					topics.append(topic_question)
-				if size>0:
-					return {
-						'statusCode': 200,
-						'body': topics
-					}
-				else:
-					return {
-						'statusCode': 205,
-						'body': "No question is added yet"
-					}
-			else:
-				return {
-					'statusCode': 400,
-					'body': 'No data found'
-				}
-		else:
-			return {
-				'statusCode': 401,
-				'body': 'Token is Invalid please re-login'
-			}
-		
-	except (TypeError, ValueError, IndexError, LookupError, SyntaxError, NameError,json.JSONDecodeError, SyntaxError ) as e:
-		return {
-			'statusCode': 400,
-			'body': f'Error: {e}'
-		}
+    try:
+
+        data = event
+        required_fields = ["token", "Topic_id"]
+        for field in required_fields:
+            if field not in data or not data[field]:
+                return {
+                    'statusCode': 400,
+                    'body': f'Error: {field} is required and cannot be empty'
+                }
+
+        if token_checker(event['token']):
+            topic_info = topic.get_item(Key={'Topic_id': event['Topic_id']})
+            if 'Item' in topic_info:
+                size = len(topic_info['Item']['question'])
+                topics = []
+                for topic_question in topic_info['Item']['question']:
+                    correct_answer = topic_question['correctAnswer']
+                    topic_question['correctAnswer'] = correct_answer.split("7481903939")[
+                        0]
+                    topics.append(topic_question)
+                if size > 0:
+                    return {
+                        'statusCode': 200,
+                        'body': topics
+                    }
+                else:
+                    return {
+                        'statusCode': 205,
+                        'body': "No question is added yet"
+                    }
+            else:
+                return {
+                    'statusCode': 400,
+                    'body': 'No data found'
+                }
+        else:
+            return {
+                'statusCode': 401,
+                'body': 'Token is Invalid please re-login'
+            }
+
+    except (TypeError, ValueError, IndexError, LookupError, SyntaxError, NameError, json.JSONDecodeError, SyntaxError) as e:
+        return {
+            'statusCode': 400,
+            'body': f'Error: {e}'
+        }
