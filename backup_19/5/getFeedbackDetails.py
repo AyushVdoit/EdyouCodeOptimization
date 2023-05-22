df612bebafa5c8bc9e3be8d527cf61724f7f3b0b
AyushVdoit <ayushjaiswal.vdoit@gmail.com>
2023-05-22 13:33:36 +0530

    "getFeedbackDetails backup"

---
+++ backup_19/5/getFeedbackDetails.py
@@ -0,0 +1,41 @@
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+dynamodb = boto3.resource('dynamodb')
+feedback=dynamodb.Table('feedback')
+Token_Data = dynamodb.Table('Token')
+def TokenChecker(token):
+    Data=Token_Data.get_item(Key={'token' : token})
+    if 'Item' in Data:
+        return True
+    else:
+        return False  
+def lambda_handler(event, context):
+    try:
+        data =event
+        required_fields = ["token"]
+        for field in required_fields:
+            if field not in data or not data[field]:
+                return {
+                'statusCode': 400,
+                'body': f'Error: {field} is required and cannot be empty'
+                }
+        if TokenChecker(event['token']):
+            feedbackData = feedback.get_item(Key={'email' :event['email'],'time':event['time']})
+            feedbackData = feedbackData['Item']
+            return {
+                    'statusCode': 200,
+                    'body': feedbackData
+                }
+        else:
+            return {
+                'statusCode': 401,
+                'body': 'Token is Invalid please re-login'
+            }
+        
+    except (TypeError, ValueError, IndexError, LookupError, SyntaxError, NameError,json.JSONDecodeError, SyntaxError ) as e:
+        return {
+            'statusCode': 400,
+            'body': f'Error: {e}'
+        }
+    
